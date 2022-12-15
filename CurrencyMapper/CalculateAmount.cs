fc1ca641dc1980896ef37f50a8330165c9fbd514
Ehtesham599 <ehtesham599@gmail.com>
2022-12-15 23:21:35 +0300

    First commit

---
+++ CurrencyMapper/CalculateAmount.cs
@@ -0,0 +1,41 @@
+﻿namespace CurrencyMapper;
+
+public interface ICalculateAmount
+{
+    public uint ConvertToInt(string currencyCode, float amount);
+
+    public float ConvertToFloat(string currencyCode, uint amount);
+}
+
+
+public class CalculateAmount
+{
+    private readonly CurrencyMap _currencyMap;
+
+    public CalculateAmount(CurrencyMap currencyMap)
+    {
+        _currencyMap = currencyMap;
+    }
+
+    public uint ConvertToInt(string currencyCode, float amount)
+    {
+        float value;
+        if (_currencyMap.Currencies.TryGetValue(currencyCode.ToUpper(), out value))
+        {
+            return Convert.ToUInt32(amount * value);
+        }
+        return Convert.ToUInt32(amount);
+    }
+
+    public float ConvertToFloat(string currencyCode, uint amount)
+    {
+        float value;
+        if (_currencyMap.Currencies.TryGetValue(currencyCode.ToUpper(), out value))
+        {
+            return Convert.ToSingle(amount / value);
+        }
+        return Convert.ToSingle(amount);
+    }
+
+}
+
---
+++ CurrencyMapper/CurrencyMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CurrencyMapper
+{
+	public class CurrencyMap
+	{
+        public readonly Dictionary<string, float> Currencies = new Dictionary<string, float>()
+        {
+            {"KWD", 1000},
+            {"OMR", 1000},
+            {"BHD", 1000},
+            {"SAR", 100},
+            {"AED", 100},
+            {"QAR", 100},
+            {"USD", 100},
+        };
+    }
+}
+
